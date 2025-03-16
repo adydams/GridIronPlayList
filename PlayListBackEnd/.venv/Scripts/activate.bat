a87f663e6236d7fc4e1f5ba13f43cb4f203aa7a8
oluiwadamilola <damilola.ogunyemi@identikosolutions.com>
2025-03-16 01:36:27 +0100

    deploying

--- PlayListBackEnd/.venv/Scripts/activate.bat
+++ PlayListBackEnd/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\somac\GridIronPlaylist\adigun-oluwadamilola-victoria\playlistbackend\.venv
+set VIRTUAL_ENV=C:\Users\somac\OneDrive\Desktop\Desktop\GridIronBE\GridIronPlayList\playlistbackend\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
